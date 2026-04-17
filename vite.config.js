1b882236b0a751227ec1517e3ed6c06543a0f33d
Suraj Singh <surajsingh@gofynd.com>
2026-04-17 11:55:10 +0530

    fix: set base for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/barcode-integration-test/',
   plugins: [react(), tailwindcss()],
   optimizeDeps: {
     exclude: ['@zxing/library', 'zxing-wasm'],
